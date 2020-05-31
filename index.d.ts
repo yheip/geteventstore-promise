42cb69a45d174a5170f01cb6cabcc5f71bbd6fe2
yheip <zyhpang@gmail.com>
2020-05-31 17:43:53 +0800

    autoAck should be boolean

--- index.d.ts
+++ index.d.ts
@@ -218,7 +218,7 @@ export class TCPClient {
 	};
 	subscribeToStream(streamName: string, onEventAppeared?: MappedEventAppearedCallback<EventStoreSubscription>, onDropped?: SubscriptionDroppedCallback<EventStoreSubscription>, resolveLinkTos?: boolean): Promise<EventStoreSubscription>;
 	subscribeToStreamFrom(streamName: string, fromEventNumber?: number, onEventAppeared?: MappedEventAppearedCallback<EventStoreCatchUpSubscription>, onLiveProcessingStarted?: LiveProcessingStartedCallback, onDropped?: SubscriptionDroppedCallback<EventStoreCatchUpSubscription>, settings?: SubscribeToStreamFromSettings): Promise<EventStoreCatchUpSubscription>;
-	connectToPersistentSubscription(streamName: string, groupName?: string, onEventAppeared?: MappedEventAppearedCallback<EventStorePersistentSubscription>, onDropped?: SubscriptionDroppedCallback<EventStorePersistentSubscription>, bufferSize?: number, autoAck?: number);
+	connectToPersistentSubscription(streamName: string, groupName?: string, onEventAppeared?: MappedEventAppearedCallback<EventStorePersistentSubscription>, onDropped?: SubscriptionDroppedCallback<EventStorePersistentSubscription>, bufferSize?: number, autoAck?: boolean);
 	close(): Promise<void>;
 	getPool(): Promise<TCPPool<object>>;
 	closeAllPools(): Promise<void>;
